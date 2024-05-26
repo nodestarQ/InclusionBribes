c12d6614a37390f42f9dc7c5c1df54c3e3d848c3
jjvalkema <jjvalkema@protonmail.com>
2024-05-26 03:11:22 +0200

    finnished reward sweeper

--- scripts/rewardSweeper.js
+++ scripts/rewardSweeper.js
@@ -1,26 +1,46 @@
-import {ethers} from 'ethers'
-import {default as inclusionBribeContractFoundryData} from '../out/AntiCensorShipBriber.sol/AntiCensorShipBriber.json'  assert {type: 'json'}
-
-const INCLUSION_BRIBE_ABI = inclusionBribeContractFoundryData.abi
-const INCLUSION_BRIBE_CONTRACT = "0xa5e74A8Be391F67B108F0aE945f720E49DF74F59"
-const PRIVATE_KEY = ""
+import { ethers } from 'ethers'
+import { default as inclusionBribeContractFoundryData } from '../out/AntiCensorShipBriber.sol/AntiCensorShipBriber.json'  assert {type: 'json'}
 
 
-const provider =  new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/2LPfLOYBTHSHfLWYSv8xib2Y7OA")
-const tx = await provider.getTransaction("0x533c42c6849772bf3a7d4fa8c79d68d29faade5e0145ba6c2c1feabd664a81a3");
-const abicoder = new ethers.AbiCoder();
-const txArgs = (abicoder.decode(['bytes', 'uint32', 'bytes32',],ethers.dataSlice(tx.data,4))).toArray()
-const hash = ethers.solidityPackedKeccak256(["bytes","uint256", "uint32", "bytes32"], [txArgs[0],tx.value, txArgs[1], txArgs[2]])
-console.log(hash)
+if (process.argv.length < 5) {
+    console.log("missing arguments\n usage: node YOUR_PRIVATEKEY INFURA_SEPOLIA_URL INCLUSION_BRIBE_CONTRACT_ADDRESS")
+} else {
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+    const INCLUSION_BRIBE_ABI = inclusionBribeContractFoundryData.abi
 
-const inclusionBribeContractObj = new ethers.Contract(
-    INCLUSION_BRIBE_CONTRACT,
-    INCLUSION_BRIBE_ABI, 
-    signer
-  );
+    const PRIVATE_KEY = process.argv[2]
+    const provider = new ethers.JsonRpcProvider(process.argv[3])
+    const INCLUSION_BRIBE_CONTRACT = process.argv[4]
+    const abicoder = new ethers.AbiCoder();
+    const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+    const inclusionBribeContractObj = new ethers.Contract(
+        INCLUSION_BRIBE_CONTRACT,
+        INCLUSION_BRIBE_ABI,
+        signer
+    );
 
-const sweepTx = await inclusionBribeContractObj.sweepReward(hash)
-console.log(sweepTx.hash)
-console.log(await sweepTx.wait(1))
+    while (true) {
+        const block = await provider.getBlock("latest")
+        console.log(Number(block.number))
+
+        for (const txHash of block.transactions) {
+            const tx = await provider.getTransaction(txHash);
+            if (tx.to === ethers.getAddress(INCLUSION_BRIBE_CONTRACT)) {
+
+                const txArgs = (abicoder.decode(['bytes', 'uint32', 'bytes32',], ethers.dataSlice(tx.data, 4))).toArray()
+                const hash = ethers.solidityPackedKeccak256(["bytes", "uint256", "uint32", "bytes32"], [txArgs[0], tx.value, txArgs[1], txArgs[2]])
+
+                const sweepTx = await inclusionBribeContractObj.sweepReward(hash)
+                console.log(`submitted sweeping tx: ${sweepTx.hash}`)
+            }
+
+        }
+
+        //is shorter then block time but better be safe and be sure to get the tx
+        //await execSync('sleep 5000');
+        await delay(5000)
+    }
+}
+
+
